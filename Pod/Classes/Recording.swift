156e9a5995fc0438d32f3a007b5577b9da0855d0
Johannes Gorset <jgorset@gmail.com>
2015-04-05 12:01:35 +0200

    Refactor with lazy property

--- Pod/Classes/Recording.swift
+++ Pod/Classes/Recording.swift
@@ -5,21 +5,35 @@ import AVFoundation
 }
 
 public class Recording : NSObject {
-    var recorder: AVAudioRecorder!
     var player: AVAudioPlayer!
     var session: AVAudioSession!
     var delegate: RecorderDelegate!
     var metering: Bool
     var url: NSURL!
     var meterLink: CADisplayLink!
-    
-    var directory: NSString {
-        return NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as NSString
-    }
-    
+
     var bitRate = 192000
     var sampleRate = 44100.0
     var channels = 1
+
+    var directory: NSString {
+        return NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as NSString
+    }
+
+    lazy var recorder: AVAudioRecorder = {
+        var recorder = AVAudioRecorder(URL: self.url, settings: [
+            AVFormatIDKey: kAudioFormatAppleLossless,
+            AVEncoderAudioQualityKey: AVAudioQuality.Max.rawValue,
+            AVEncoderBitRateKey: self.bitRate,
+            AVNumberOfChannelsKey: self.channels,
+            AVSampleRateKey: self.sampleRate
+        ], error: nil)
+
+        recorder.delegate = self.delegate
+        recorder.meteringEnabled = self.metering
+
+        return recorder
+    }()
 
     public init(to: NSString, on: RecorderDelegate, withMetering:Bool = false)
     {
@@ -34,38 +48,26 @@ public class Recording : NSObject {
     
     public func prepare()
     {
-        self.recorder = AVAudioRecorder(URL: url, settings: [
-            AVFormatIDKey: kAudioFormatAppleLossless,
-            AVEncoderAudioQualityKey: AVAudioQuality.Max.rawValue,
-            AVEncoderBitRateKey: bitRate,
-            AVNumberOfChannelsKey: channels,
-            AVSampleRateKey: sampleRate
-        ], error: nil)
-
-        recorder.delegate = delegate
-        
-        recorder.prepareToRecord()
-        
-        if metering {
-            recorder.meteringEnabled = metering
-            startMetering()
-        }
+        // This feels a weird thing to do, but since Swift won't let you reference properties
+        // without assigning them and I really need to just hit this lazy property to compute it
+        // I'ma go ahead and just assign it to an arbitrary variable.
+        var prepared = recorder
     }
 
     public func record()
     {
-        if recorder == nil {
-            prepare()
-        }
-
         self.session.setCategory(AVAudioSessionCategoryRecord, error: nil)
+
+        recorder.prepareToRecord()
+
+        startMetering()
         recorder.record()
     }
     
     public func stop()
     {
         recorder.stop()
-        
+
         if metering {
             stopMetering()
         }
